ffdce1059e50c6036f2c83d84963f55c4242bf34
gitinfra2018 <44669728+gitinfra2018@users.noreply.github.com>
2019-11-04 12:37:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,6 @@ echo "Il est l'heure de manger";
 echo "Ce soir, c'est soupe aux champignons";
 echo "Coucou";
 echo "Salut à vous";
-   
+   echo "Le site des bonnes nouvelles à changé ma vie, depuis le soleil est rayonnant";
+
    ?></p>
