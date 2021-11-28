fc87c2620c3ed530c347167154e17dd1476f7897
gitinfra2018 <44669728+gitinfra2018@users.noreply.github.com>
2021-11-28 22:27:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php
 echo "Hello world";
-echo "Il est l'heure de manger";
-echo "Ce soir, c'est soupe aux champignons";
-echo "Coucou";
-echo "Salut à vous";
-   echo "Le site des bonnes nouvelles à changé ma vie, depuis le soleil est rayonnant";
-
-   echo "<h1> VIVE LES CACAHUÈTES </h1>";
-
-echo "HAHA";
-echo "<h1><3</h1>";
-echo "belle journée aujourd'hui"
-   
    
 ?>
